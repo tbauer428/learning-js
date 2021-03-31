b301dd14f034de212d811e46e169aaa9f1128210
Thomas <tom.bauer95@gmail.com>
2021-03-30 23:15:17 -0500

    bank-account

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,20 +1,49 @@
 export class BankAccount {
-  constructor() {}
+  constructor() {
+    this.isOpen = false;
+  }
 
   open() {
+    if (this.isOpen) {
+      throw new ValueError();
+    }
+    this.isOpen = true;
     this.balance = 0;
   }
 
-  close() {}
+  close() {
+    if (!this.isOpen) {
+      throw new ValueError();
+    }
+    this.isOpen = false;
+    this.balance = 0;
+  }
 
-  deposit() {}
+  deposit(value) {
+    if (!this.isOpen || value < 0) {
+      throw new ValueError();
+    }
 
-  withdraw() {}
+    this.balance += value;
+  }
 
-  get balance() {}
+  withdraw(value) {
+    if (value < 0 || value > this.balance) {
+      throw new ValueError();
+    }
+
+    this.balance -= value;
+  }
+
+  get balance() {
+    if (!this.isOpen) {
+      throw new ValueError();
+    }
+    return this._balance;
+  }
 
   set balance(balance) {
-    return this.balance = balance;
+    this._balance = balance;
   }
 }
 
