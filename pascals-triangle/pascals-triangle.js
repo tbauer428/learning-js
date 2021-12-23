41b00f23074051e24e136a0d5450179c06a92de1
Thomas Bauer <tom.bauer95@gmail.com>
2021-12-22 22:50:09 -0600

    solved

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,16 +1,25 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const rows = (numberOfRows) => {
-  let parentArray = [];
+  let answer = [[1], [1, 1]]
 
-  for (let i = 0; i < numberOfRows; i++) {
-    let childArray = Math.pow(11, i).toString().split('');
-
-    parentArray.push(childArray.map((child) => parseInt(child)));
+  if (numberOfRows === 0) {
+    return []
+  } else if (numberOfRows === 1) {
+    return [[1]]
+  } else if (numberOfRows === 2) {
+    return answer
+  } else {
+    for (let i = 2; i < numberOfRows; i++) {
+      let prev = answer[answer.length - 1]
+      let nextRow = [1]
+      for (let o = 0; o < prev.length - 1; o++) {
+        let current = prev[o]
+        let next = prev[o + 1]
+        nextRow.push(current + next)
+      }
+      nextRow.push(1)
+      answer.push(nextRow)
+    }
   }
 
-  return numberOfRows === 0 ? [] : parentArray;
-};
+  return answer
+}
